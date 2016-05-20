25ea4c51ecba4b396e519fc50a11d6399fd3a6f5
jingguang.chen <jingguang.chen@dianping.com>
2016-05-20 20:43:02 +0800

    init bean

---
+++ src/main/java/blog/brucefeng/info/bean/Bar.java
@@ -0,0 +1,4 @@
+package blog.brucefeng.info.bean;
+
+public class Bar {
+}
---
+++ src/main/java/blog/brucefeng/info/bean/Foo.java
@@ -0,0 +1,22 @@
+package blog.brucefeng.info.bean;
+
+public class Foo {
+    private String name;
+    private Bar bar;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bar getBar() {
+        return bar;
+    }
+
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+}
